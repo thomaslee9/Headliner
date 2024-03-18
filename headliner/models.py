5349df0762d09906f5d8973a37a98352f85ae09b
ikotecha <ikotecha@andrew.cmu.edu>
2024-03-17 20:52:10 -0400

    added forms and begun global page

--- headliner/models.py
+++ headliner/models.py
@@ -6,6 +6,12 @@ from django.db import models
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
+class Event(models.Model):
+    event_description = models.CharField(blank=True, max_length=500, verbose_name='New Post')
+    created_by    = models.ForeignKey(User, on_delete=models.PROTECT, related_name="entry_creators")
+    creation_time = models.DateTimeField()
 
+    def __str__(self):
+        return f"Entry(id={self.id})"
 
 
--- headliner/urls.py
+++ headliner/urls.py
@@ -5,4 +5,5 @@ from django.conf import settings
 urlpatterns = [
     path('', views.login_action, name="login"),
     path('register/', views.register_action, name="register"),
+    path('global/', views.global_action, name="global"),
 ]
