e7c8cdb82ed0d5134a394f9343927e7763e36c78
ikotecha <ikotecha@andrew.cmu.edu>
2024-03-17 20:33:25 -0400

    added image links and initial urls

---
+++ headliner/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.login_action, name="login"),
+    path('register/', views.register_action, name="register"),
+]
